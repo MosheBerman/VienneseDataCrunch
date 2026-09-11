e8ee2bd9e4ba7a35cc2cec61e87fe725dec76b72
Pikachu <pikachu@local>
2026-09-11 01:57:37 +0000

    Fix dead taps: parse stringified member-id arrays from vector tiles

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lehmann1938-20260911014739';
+const CACHE='lehmann1938-20260911015735';
 const SHELL=['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
